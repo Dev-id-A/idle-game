90dd4577931275fd7833a807144e5a203bf40fde
Dev-id-A <davidaranzana618@gmail.com>
2025-12-30 13:26:05 +0100

    First upgrade created

---
+++ src/assets/UpgradesJson.ts
@@ -0,0 +1,12 @@
+export const upgrades = 
+    [
+        {
+            name: "Computer",
+            src: "SVG/computer.svg",
+            description: "A computer programmed to send automatic mails.",
+            mailsSended: 5,
+            interval: 3000,
+            cost: 50,
+            id: 1
+        }
+    ]
