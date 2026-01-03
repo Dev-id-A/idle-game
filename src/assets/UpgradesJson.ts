07a58e9c0c5ba122d0ed0d0605a418cc2d2ee154
Dev-id-A <davidaranzana618@gmail.com>
2026-01-03 10:38:26 +0100

    Second upgrade created

--- src/assets/UpgradesJson.ts
+++ src/assets/UpgradesJson.ts
@@ -5,9 +5,19 @@ export const upgrades =
             src: "SVG/computer.svg",
             description: "A computer programmed to send automatic mails.",
             mailsSended: 5,
-            interval: 3000,
+            interval: 1500,
             cost: 50,
             id: 1,
+            actualLevel: 1
+        },
+        {
+            name: "Worker",
+            src: "SVG/worker.svg",
+            description: "Your first worker ready to send that electronics messages.",
+            mailsSended: 25,
+            interval: 5000,
+            cost: 200,
+            id: 2,
             actualLevel: 1
         }
     ]
--- src/assets/UpgradesSection.tsx
+++ src/assets/UpgradesSection.tsx
@@ -2,7 +2,7 @@ import { upgrades } from "./UpgradesJson"
 
 function UpgradesSection() {
   return (
-    <section className="p-2 w-full">
+    <section className="flex flex-col gap-2 p-2 w-full">
         {upgrades.map((upgrade)=>{
             return (
             <div className="border-2 flex flex-row items-center rounded-sm text-[20px] py-2" key={upgrade.id}>
@@ -11,10 +11,10 @@ function UpgradesSection() {
                     <img className="border-1 w-16 h-16" src={upgrade.src} alt={upgrade.name} />
                 </div>
 
-                <div className="w-full flex flex-col pl-5">
+                <div className="w-full flex flex-col gap-2 px-5">
                     <h2>{upgrade.name}</h2>
                     <h3>{upgrade.mailsSended} m/s</h3>
-                    <div>{upgrade.interval}</div>
+                    <h3>{upgrade.interval}</h3>
                     <button className="border-2 rounded-md px-3 py-1 bg-green-300 w-1/2">
                         {upgrade.actualLevel > 1 ? "Upgrade":"Buy"} {upgrade.cost}</button>
                 </div>
