36f1d3fed6db8380ba50a855852b0d5b2c7e765d
Dev-id-A <davidaranzana618@gmail.com>
2026-01-08 15:50:02 +0100

    Team upgrade added

--- src/assets/UpgradesJson.ts
+++ src/assets/UpgradesJson.ts
@@ -9,7 +9,7 @@ export const upgrades =
             actualTime: 0,
             cost: 50,
             id: 1,
-            actualLevel: 1
+            actualLevel: 0
         },
         {
             name: "Worker",
@@ -20,6 +20,17 @@ export const upgrades =
             actualTime: 0,
             cost: 200,
             id: 2,
-            actualLevel: 1
+            actualLevel: 0
+        },
+        {
+            name: "Team",
+            src: "SVG/team.svg",
+            description: "An experienced team dedicated with love to every email.",
+            mailsSended: 150,
+            interval: 10000,
+            actualTime: 0,
+            cost: 2500,
+            id: 3,
+            actualLevel: 0
         }
     ]
